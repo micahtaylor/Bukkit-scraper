f73f4814c0adfd05415c19ed6f038572b2c44c1b
Micah Taylor <micah.taylor@gsa.gov>
2018-02-05 16:42:44 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Bukkit scraper
+
+I always wanted to scrape bukk.it and have a pictoral directory of it.
+
+Borrowing from https://dev.to/aurelkurtula/introduction-to-web-scraping-with-nodejs-9h2
---
+++ bukkitList.js
@@ -0,0 +1,22 @@
+let axios = require('axios');
+let cheerio = require('cheerio');
+let fs = require('fs');
+
+axios.get('https://bukk.it/')
+    .then((response) => {
+        if(response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            let bukkitList = [];
+            $('td a').each(function(i, elem) {
+                bukkitList[i] = {
+                    title: $(this).text().trim(),
+                    url: $(this).attr('href')
+                }
+            });
+            const bukkitListTrimmed = bukkitList.filter(n => n != undefined )
+            fs.writeFile('bukkitList.json',
+                          JSON.stringify(bukkitListTrimmed, null, 4),
+                          (err)=> console.log('File successfully written!'))
+    }
+}, (error) => console.log(err) );
